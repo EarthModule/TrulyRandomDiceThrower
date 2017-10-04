0c0a611bb4124e9b634c8ca9f3e0b457ceee2da3
Toni Nurmi <nurmi.toni.s@gmail.com>
2017-10-04 13:18:04 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='RandomDiceThrower',
+    version='1.0.0',
+    packages=['truerandomdice'],
+    url='',
+    license='',
+    author='toni nurmi',
+    author_email='toni.nurmi@hotmail.com',
+    description=''
+)
