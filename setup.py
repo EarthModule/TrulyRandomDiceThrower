c2716787be22499b474b078daff3fde3c1dbbf7c
Toni Nurmi <nurmi.toni.s@gmail.com>
2017-10-04 19:12:08 +0300

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from distutils.core import setup
 
 setup(
     name='truerandomdice',
-    version='0.1',
+    version='0.2',
     packages=['truerandomdice'],
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7',
-        'Topic :: Random :: Dice :: RPG',
+        'Topic :: Random :: Dice',
     ],
     url='https://github.com/EarthModule/TrulyRandomDiceThrower',
-    download_url='https://github.com/EarthModule/TrulyRandomDiceThrower/releases/tag/0.1',
+    download_url='https://github.com/EarthModule/TrulyRandomDiceThrower/releases/tag/0.2',
     license='',
     author='toni nurmi',
     author_email='toni.nurmi@hotmail.com',
